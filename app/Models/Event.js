12bcfec8ab514e5b4006e5cb6653ad3182c09a46
Breno França <franciscobreno.si@gmail.com>
2019-01-10 01:46:41 -0300

    event_date_format - adicionado hook para formatar padrao da data

--- app/Models/Event.js
+++ app/Models/Event.js
@@ -4,14 +4,22 @@ const Model = use('Model')
 const moment = require('moment')
 
 class Event extends Model {
-  isExpired () {
-    return moment()
-      .subtract('1', 'days')
-      .isAfter(this.date)
+  static boot () {
+    super.boot()
+
+    this.addHook('afterFind', async instance => {
+      instance.date = moment(instance.date).format('DD/MM/YYYY HH:mm')
+    })
   }
 
   user () {
     return this.belongsTo('App/Models/User')
+  }
+
+  isExpired () {
+    return moment()
+      .subtract('1', 'days')
+      .isAfter(this.date)
   }
 }
 
